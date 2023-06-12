abec7c80ba442e904f8744ab4ac33eca91ed74ba
PCA1976 <69221542+PCA1976@users.noreply.github.com>
2023-06-12 08:52:20 -0300

    Update SaludoTeam6.java

--- src/main/java/com/grupo6/controllers/SaludoTeam6.java
+++ src/main/java/com/grupo6/controllers/SaludoTeam6.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api6")
 public class SaludoTeam6 {
     @GetMapping("/SaludoTeam6")
     public String saludo(){
