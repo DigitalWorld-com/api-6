7ccb2d69263c7a48b1718b092d5665c71e9cac25
PCA1976 <pauloarguello@gmail.com>
2023-07-19 17:14:07 -0300

    Logrado el ingreso inicial del set de datos a la tabla desde la URL. Y generada un incremental de consultas llamado "ingresos".

--- src/main/java/com/digitalworlds/api6/base/entities/MuseoEntity.java
+++ src/main/java/com/digitalworlds/api6/base/entities/MuseoEntity.java
@@ -1,13 +1,15 @@
 package com.digitalworlds.api6.base.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 
 @Getter
 @Setter
 @RequiredArgsConstructor
-@Entity(name="Museos")
+@Entity(name = "Museos")
 public class MuseoEntity<M> {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +17,5 @@ public class MuseoEntity<M> {
     private Long pk;
     private Long id;
     private String nombre;
+    private Long ingresos;
 }
